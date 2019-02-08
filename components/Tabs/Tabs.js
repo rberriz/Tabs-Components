b3d0e06c869d0bc0b967d2aa96a5402f7474df5a
rberriz <rberriz@icloud.com>
2019-02-07 20:56:45 -0500

    progress tabs

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -19,16 +19,16 @@ class TabLink {
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
+    const links = document.querySelectorAll('.tabs-links .tabs-link');;
 
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
-
+    Array.from(links).forEach(element => {element.classList.remove('tabs-link-selected')});
+  
     // Add a class named "tabs-link-selected" to this link
-    // this.element;
+    this.element.classList.toggle('tabs-link-selected');
     
     // Call the select method on the item associated with this link
-
+    
   }
 }
 
@@ -38,14 +38,14 @@ class TabItem {
     this.element = element;
   }
 
-  select() {
+  select() { 
     // Select all ".tabs-item" elements from the DOM
-    // const items;
+    const items =  document.querySelectorAll('.tabs-items .tabs-item');
 
     // Remove the class "tabs-item-selected" from each element
-    
+    items.forEach(element => {element.classList.remove('tabs-item-selected')})
     // Add a class named "tabs-item-selected" to this element
-    //this.element;
+    this.element.classList.toggle('tabs-item-selected');
   }
 }
 
@@ -59,4 +59,4 @@ class TabItem {
 
 */
 
-links = document.querySelectorAll();
+links = document.querySelectorAll('.tabs-links .tabs-link').forEach(link => new TabLink(tab));
