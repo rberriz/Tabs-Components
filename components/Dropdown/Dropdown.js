ca3273dadf66c347824e049e019f37099b6a7b0d
rberriz <rberriz@icloud.com>
2019-02-07 21:51:12 -0500

    MVP done

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,18 +1,18 @@
 class Dropdown {
   constructor(element) {
     
-    // Assign this.element to the dropdown elementt
-    this.element;
+    // Assign this.element to the dropdown element
+    this.element = element;
     
     // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
-    this.button = this.element.querySelector();
+    this.button = this.element.querySelector('.dropdown .dropdown-button');
     
     // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content;
+    this.content = this.element.querySelector('.dropdown-content');
     
     // Add a click handler to the button reference and call the toggleContent method.
     this.button.addEventListener('click', () => {
-
+      this.content.classList.toggle('dropdown-hidden')
     })
   }
 
